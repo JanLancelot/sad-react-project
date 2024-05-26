61d39cb3c1638efc19a9f18b0ebf29577d519391
Jan Lancelot Mailig <maypagasaml@gmail.com>
2024-05-27 00:02:13 +0800

    Added view custom eval

--- src/pages/ViewEvalForm.js
+++ src/pages/ViewEvalForm.js
@@ -126,10 +126,12 @@ const ViewEvalForm = () => {
             essayQuestions.length > 0 &&
             essayQuestions.map((question, index) => (
               <div key={index} className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2">
+                  {question}
+                </label>
                 <textarea
-                  name={`essayQuestion${index}`}
-                  placeholder={question}
-                  value={formData[`essayQuestion${index}`]}
+                  name={`essayAnswers[${index}]`}
+                  value={formData.essayAnswers[index] || ""}
                   disabled
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 />
