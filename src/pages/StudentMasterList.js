5054fbb42ef5bb03d2df56b67f2002fb5feb504f
Jan Lancelot Mailig <maypagasaml@gmail.com>
2024-05-28 13:25:06 +0800

    Fixed data issue

--- src/pages/StudentMasterList.js
+++ src/pages/StudentMasterList.js
@@ -1,75 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { db } from "../firebaseConfig";
-import ReactToPrint from "react-to-print";
+import React, { useState, useEffect } from 'react';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
+import ReactToPrint from 'react-to-print';
 
 const departmentOptions = [
-  { id: 0, name: "Select Department", color: "" },
-  {
-    id: 1,
-    name: "Computer Studies",
-    color: "bg-blue-400",
-    dbl: "CS department",
-  },
-  {
-    id: 2,
-    name: "Education",
-    color: "bg-green-400",
-    dbl: "Education Department",
-  },
-  {
-    id: 3,
-    name: "Accountancy",
-    color: "bg-yellow-400",
-    dbl: "Accountancy Department",
-  },
-  {
-    id: 4,
-    name: "Business Administration",
-    color: "bg-purple-400",
-    dbl: "Business Administration Department",
-  },
-  {
-    id: 5,
-    name: "Arts and Sciences",
-    color: "bg-pink-400",
-    dbl: "Arts and Sciences Department",
-  },
-  { id: 6, name: "Maritime", color: "bg-teal-400", dbl: "Maritime department" },
-  {
-    id: 7,
-    name: "Health Sciences",
-    color: "bg-red-400",
-    dbl: "Health Sciences Department",
-  },
-  {
-    id: 8,
-    name: "Hospitality",
-    color: "bg-orange-400",
-    dbl: "Hospitality Management and Tourism Department",
-  },
+  { id: 0, name: 'Select Department', color: '' },
+  { id: 1, name: 'Computer Studies', color: 'bg-blue-400', dbl: 'CS department' },
+  { id: 2, name: 'Education', color: 'bg-green-400', dbl: 'Education Department' },
+  { id: 3, name: 'Accountancy', color: 'bg-yellow-400', dbl: 'Accountancy Department' },
+  { id: 4, name: 'Business Administration', color: 'bg-purple-400', dbl: 'Business Administration Department' },
+  { id: 5, name: 'Arts and Sciences', color: 'bg-pink-400', dbl: 'Arts and Sciences Department' },
+  { id: 6, name: 'Maritime', color: 'bg-teal-400', dbl: 'Maritime department' },
+  { id: 7, name: 'Health Sciences', color: 'bg-red-400', dbl: 'Health Sciences Department' },
+  { id: 8, name: 'Hospitality', color: 'bg-orange-400', dbl: 'Hospitality Management and Tourism Department' },
 ];
 
 function StudentMasterlist() {
   const [students, setStudents] = useState([]);
   const [filteredStudents, setFilteredStudents] = useState([]);
-  const [department, setDepartment] = useState("");
+  const [department, setDepartment] = useState('');
 
   useEffect(() => {
     const fetchStudents = async () => {
-      const q = collection(db, "users");
+      const q = collection(db, 'users');
       const querySnapshot = await getDocs(q);
       const studentsData = querySnapshot.docs
         .map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }))
-        .filter(
-          (student) =>
-            !student.adRole ||
-            student.adRole === "SSC" ||
-            student.adRole === "ssc"
-        );
+        .filter((student) => student.department);
       setStudents(studentsData);
       setFilteredStudents(studentsData);
     };
