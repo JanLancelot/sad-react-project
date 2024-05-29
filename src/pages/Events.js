84bb1d0b2d3cdf699369f2ffcfea2ed27bb41fb8
Jan Lancelot Mailig <maypagasaml@gmail.com>
2024-05-29 08:49:03 +0800

    Modified events

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,44 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../firebaseConfig';
-import ReactToPrint from 'react-to-print';
+import React, { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import ReactToPrint from "react-to-print";
 
 const departmentOptions = [
-  { id: 0, name: 'Select Department', color: '', dbl: '' },
-  { id: 1, name: 'Computer Studies', color: 'bg-blue-400', dbl: 'CS department' },
-  { id: 2, name: 'Education', color: 'bg-green-400', dbl: 'Education Department' },
-  { id: 3, name: 'Accountancy', color: 'bg-yellow-400', dbl: 'Accountancy Department' },
-  { id: 4, name: 'Business Administration', color: 'bg-purple-400', dbl: 'Business Administration Department' },
-  { id: 5, name: 'Arts and Sciences', color: 'bg-pink-400', dbl: 'Arts and Sciences Department' },
-  { id: 6, name: 'Maritime', color: 'bg-teal-400', dbl: 'Maritime department' },
-  { id: 7, name: 'Health Sciences', color: 'bg-red-400', dbl: 'Health Sciences Department' },
-  { id: 8, name: 'Hospitality', color: 'bg-orange-400', dbl: 'Hospitality Management and Tourism Department' },
+  { id: 0, name: "Select Department", color: "", dbl: "" },
+  {
+    id: 1,
+    name: "Computer Studies",
+    color: "bg-blue-400",
+    dbl: "CS department",
+  },
+  {
+    id: 2,
+    name: "Education",
+    color: "bg-green-400",
+    dbl: "Education Department",
+  },
+  {
+    id: 3,
+    name: "Accountancy",
+    color: "bg-yellow-400",
+    dbl: "Accountancy Department",
+  },
+  {
+    id: 4,
+    name: "Business Administration",
+    color: "bg-purple-400",
+    dbl: "Business Administration Department",
+  },
+  {
+    id: 5,
+    name: "Arts and Sciences",
+    color: "bg-pink-400",
+    dbl: "Arts and Sciences Department",
+  },
+  { id: 6, name: "Maritime", color: "bg-teal-400", dbl: "Maritime department" },
+  {
+    id: 7,
+    name: "Health Sciences",
+    color: "bg-red-400",
+    dbl: "Health Sciences Department",
+  },
+  {
+    id: 8,
+    name: "Hospitality",
+    color: "bg-orange-400",
+    dbl: "Hospitality Management and Tourism Department",
+  },
 ];
 
 function Events() {
   const [events, setEvents] = useState([]);
   const [filteredEvents, setFilteredEvents] = useState([]);
-  const [department, setDepartment] = useState('');
-  const [eventFilter, setEventFilter] = useState('all');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [sortOrder, setSortOrder] = useState('asc');
+  const [department, setDepartment] = useState("");
+  const [eventFilter, setEventFilter] = useState("all");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
 
   useEffect(() => {
     const fetchEvents = async () => {
-      const q = collection(db, 'meetings');
+      const q = collection(db, "meetings");
       const querySnapshot = await getDocs(q);
-      const eventsData = await Promise.all(querySnapshot.docs.map(async (doc) => {
-        const evaluationsRef = collection(db, 'meetings', doc.id, 'evaluations');
-        const evaluationsSnapshot = await getDocs(evaluationsRef);
-        const ratings = evaluationsSnapshot.docs.map(evaluation => evaluation.data().averageRating);
-        const averageScore = ratings.length ? (ratings.reduce((a, b) => a + b, 0) / ratings.length).toFixed(2) : 'N/A';
-        return {
-          id: doc.id,
-          ...doc.data(),
-          averageScore: parseFloat(averageScore),
-        };
-      }));
+      const eventsData = await Promise.all(
+        querySnapshot.docs.map(async (doc) => {
+          const evaluationsRef = collection(
+            db,
+            "meetings",
+            doc.id,
+            "evaluations"
+          );
+          const evaluationsSnapshot = await getDocs(evaluationsRef);
+          const ratings = evaluationsSnapshot.docs.map(
+            (evaluation) => evaluation.data().averageRating
+          );
+          const averageScore = ratings.length
+            ? (ratings.reduce((a, b) => a + b, 0) / ratings.length).toFixed(2)
+            : "N/A";
+
+          const attendees = doc.data().attendees || [];
+          const numberOfAttendees = attendees.length;
+
+          return {
+            id: doc.id,
+            ...doc.data(),
+            averageScore: parseFloat(averageScore),
+            numberOfAttendees,
+          };
+        })
+      );
       setEvents(eventsData);
       setFilteredEvents(eventsData);
     };
@@ -50,7 +101,7 @@ function Events() {
     const selectedDepartment = e.target.value;
     setDepartment(selectedDepartment);
 
-    if (selectedDepartment === '') {
+    if (selectedDepartment === "") {
       setFilteredEvents(events);
     } else {
       const filtered = events.filter(
@@ -64,23 +115,25 @@ function Events() {
     const filterValue = e.target.value;
     setEventFilter(filterValue);
 
-    if (filterValue === 'all') {
+    if (filterValue === "all") {
       setFilteredEvents(events);
-    } else if (filterValue === 'finished') {
+    } else if (filterValue === "finished") {
       const filtered = events.filter(
-        (event) => new Date(event.date.split('/').reverse().join('-')) < new Date()
+        (event) =>
+          new Date(event.date.split("/").reverse().join("-")) < new Date()
       );
       setFilteredEvents(filtered);
-    } else if (filterValue === 'upcoming') {
+    } else if (filterValue === "upcoming") {
       const filtered = events.filter(
-        (event) => new Date(event.date.split('/').reverse().join('-')) > new Date()
+        (event) =>
+          new Date(event.date.split("/").reverse().join("-")) > new Date()
       );
       setFilteredEvents(filtered);
-    } else if (filterValue === 'between') {
+    } else if (filterValue === "between") {
       const start = new Date(startDate);
       const end = new Date(endDate);
       const filtered = events.filter((event) => {
-        const eventDate = new Date(event.date.split('/').reverse().join('-'));
+        const eventDate = new Date(event.date.split("/").reverse().join("-"));
         return eventDate >= start && eventDate <= end;
       });
       setFilteredEvents(filtered);
@@ -89,24 +142,24 @@ function Events() {
 
   const handleStartDateChange = (e) => {
     setStartDate(e.target.value);
-    handleEventFilterChange({ target: { value: 'between' } });
+    handleEventFilterChange({ target: { value: "between" } });
   };
 
   const handleEndDateChange = (e) => {
     setEndDate(e.target.value);
-    handleEventFilterChange({ target: { value: 'between' } });
+    handleEventFilterChange({ target: { value: "between" } });
   };
 
   const handleSortByScore = () => {
     const sortedEvents = [...filteredEvents].sort((a, b) => {
-      if (sortOrder === 'asc') {
+      if (sortOrder === "asc") {
         return a.averageScore - b.averageScore;
       } else {
         return b.averageScore - a.averageScore;
       }
     });
     setFilteredEvents(sortedEvents);
-    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
   };
 
   const componentRef = React.createRef();
@@ -121,11 +174,7 @@ function Events() {
           className="border border-gray-300 rounded px-2 py-1 mr-4"
         >
           {departmentOptions.map((option) => (
-            <option
-              key={option.id}
-              value={option.dbl}
-              className={option.color}
-            >
+            <option key={option.id} value={option.dbl} className={option.color}>
               {option.name}
             </option>
           ))}
@@ -143,7 +192,7 @@ function Events() {
           <option value="between">Events Between Dates</option>
         </select>
 
-        {eventFilter === 'between' && (
+        {eventFilter === "between" && (
           <div>
             <label className="mr-2">Start Date:</label>
             <input
@@ -174,31 +223,40 @@ function Events() {
       />
 
       <div ref={componentRef}>
-        <table className="table-auto w-full border-collapse">
+        <table className="min-w-full divide-y divide-gray-200">
           <thead>
             <tr>
-              <th className="px-4 py-2 border">Event Name</th>
-              <th className="px-4 py-2 border">Description</th>
-              <th className="px-4 py-2 border">Date</th>
-              <th className="px-4 py-2 border">Category</th>
-              <th className="px-4 py-2 border">Department</th>
-              <th className="px-4 py-2 border cursor-pointer" onClick={handleSortByScore}>
-                Average Score
-                {sortOrder === 'asc' ? ' 🔼' : ' 🔽'}
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Event Name
               </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Department
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Date
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Number of Attendees
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Average Score
+              </th>{" "}
             </tr>
           </thead>
-          <tbody>
+          <tbody className="bg-white divide-y divide-gray-200">
             {filteredEvents.map((event) => (
               <tr key={event.id}>
-                <td className="border px-4 py-2">{event.name}</td>
-                <td className="border px-4 py-2">{event.description}</td>
-                <td className="border px-4 py-2">
-                  {new Date(event.date.split('/').reverse().join('-')).toLocaleDateString()}
+                <td className="px-6 py-4 whitespace-nowrap">{event.name}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {event.department}
                 </td>
-                <td className="border px-4 py-2">{event.category}</td>
-                <td className="border px-4 py-2">{event.department}</td>
-                <td className="border px-4 py-2">{event.averageScore}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{event.date}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {event.numberOfAttendees}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {event.averageScore}
+                </td>{" "}
               </tr>
             ))}
           </tbody>
